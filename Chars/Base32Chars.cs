d7b4a22cdf88b2694632692a539e5cbe03b10b85
Grey Wind <wjunqin7@gmail.com>
2024-08-29 18:02:29 +0800

    1.0.0 beta 1 (#1)
    * Init
    
    * create project
    
    * BTO
    
    - Base 32

---
+++ Chars/Base32Chars.cs
@@ -0,0 +1,43 @@
+﻿namespace Codec.Chars
+{
+    /// <summary>
+    /// Base32 chars.
+    /// </summary>
+    public class Base32Chars
+    {
+        /// <summary>
+        /// RFC 4648 Base32 alphabet.
+        /// </summary>
+        public static readonly char[] RFC4648 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ234567".ToCharArray();
+
+        /// <summary>
+        /// Extended Hex Base32 alphabet, used for encoding hexadecimal values.
+        /// </summary>
+        public static readonly char[] ExtendedHex = "0123456789ABCDEFGHIJKLMNOPQRSTUV".ToCharArray();
+
+        /// <summary>
+        /// ZBase32 alphabet, used in ZBase32 encoding scheme.
+        /// </summary>
+        public static readonly char[] ZBase32 = "ybndrfg8ejkmcpqxot1uwisza345h768".ToCharArray();
+
+        /// <summary>
+        /// Crockford Base32 alphabet, designed to avoid visually ambiguous characters.
+        /// </summary>
+        public static readonly char[] Crockford = "0123456789abcdefghjkmnpqrstvwxyz".ToCharArray();
+
+        /// <summary>
+        /// Electrologica Base32 alphabet, used in Electrologica encoding scheme.
+        /// </summary>
+        public static readonly char[] Electrologica = "ABCDEFGHIJKLMNOPQRSTUVWXYZ234567".ToCharArray();
+
+        /// <summary>
+        /// GeoHash Base32 alphabet, used in GeoHash encoding for geographical coordinates.
+        /// </summary>
+        public static readonly char[] GeoHash = "0123456789bcdefghjkmnpqrstuvwxyz".ToCharArray();
+
+        /// <summary>
+        /// Word-safe Base32 alphabet, designed to avoid ambiguous characters and be easy to read.
+        /// </summary>
+        public static readonly char[] WordSafe = "abcdefghijklmnopqrstuvwxyz234567".ToCharArray();
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# QingYi.Codec
+# QingYi.Crypto
+
+[![NuGet](https://img.shields.io/nuget/dt/QingYi.Codec.svg)](https://www.nuget.org/packages/QingYi.Codec) [![NuGet](https://img.shields.io/nuget/vpre/QingYi.Codec.svg)](https://www.nuget.org/packages/QingYi.Codec)
+
+A simple library for converting byte arrays to strings.
+
+For business please send Issue or contact us directly.
+
+
+
+## Installing
+
+.NET Cli
+
+```sh
+dotnet add package QingYi.Codec
+```
+
+Package Manager
+
+```TEXT
+NuGet\Install-Package QingYi.Codec
+```
+
+## Feedback 
+
+For bug reporting/requests you can report issues on [github](https://github.com/QingYi-Studio/QingYi.Codec/issue) if required.
+
+## Finally
+
+Enjoy!
